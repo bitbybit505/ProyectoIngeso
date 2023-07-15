2cea21d1b2ae56cfb52d4b002569ec69c82b48be
pinhasaurio <67079019+pinhasaurio@users.noreply.github.com>
2023-07-14 23:50:09 -0400

    login estilizado boostrap

--- login.php
+++ login.php
@@ -37,7 +37,6 @@
   }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -47,52 +46,73 @@
     
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    
     <title>Login</title>
+    
+    <style>
+        body{
+        background: #ffe259;
+        background: linear-gradient(to right, #FF0000, #FFFF00);
+        }
+        .bg{
+            background-image: url("assets/electric.jpg");
+            backgroind-position: center center;
+        }
+    </style>
 </head>
 <body>
+    
     <?php if(!empty($error_message)) { ?>   
         <div id=errorMessage>
           <strong>Error: <?= $error_message ?> </strong>
         </div>
     <?php } ?>
-  
-  <div class="nav-bar">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand fw-semibold" href="index.php">LogoThingy</a>
-      </div>
-    </nav>
-  </div>
-  <div class="container pt-5 mt-5">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <div class="card">
-          <div class="card-header fw-semibold ">Login</div>
-          <div class="card-body">
-            <form action="login.php" method="POST">
-              <div class="form-group mb-3">
-                <label for="username" class="mb-1">Username</label>
-                <input type="username" name="username" class="form-control" id="username" aria-describedby="usernameHelp" placeholder="Enter username">
-              </div>
-              <div class="form-group mb-3">
-                <label for="password" class="mb-1">Password</label>
-                <input type="password" name="password" class="form-control" id="password" placeholder="Password">
-              </div>
-              <div class="form-check mb-3">
-                <input type="checkbox" class="form-check-input " id="remember">
-                <label class="form-check-label" for="remember">Remember me</label>
-              </div>
-              <button type="submit" class="btn btn-primary">Login</button>
-              <div class="mt-2">
-                <a href="forgot-password.php">Forgot Password?</a>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</body>
+    <div class="nav-bar">
+        <nav class="navbar navbar-expand-lg bg-black">
 
+            <div class="container-fluid">
+                <a class="navbar-brand fw-semibold text-white" href="index.php">Casa Flores</a>
+            </div>
+        </nav>
+    </div>
+    <div class="container w-100 bg-primary mt-5 rounded shadow"> 
+        <div class="row align-items-stretch">
+            <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded">
+                <!-- imagen-->
+                
+                <!--background: linear-gradient(to right,#ffa751, #ffe259)-->
+
+            </div>
+            <div class="col bg-white p-5 rounded-end">
+                <div class="text-end" style="height: 60px;bottom: 7%;position: relative;">
+                    <img src="assets/casaflores.jpg" width="150" alt="" style="padding-bottom: 9px;">
+                </div>
+                <h2 class="fw-bold text-center py-5" >Bienvenido</h2>
+                <!-- login -->
+                <form action="login.php" method="POST">
+                    <div class="mb-4">
+                        <label for="username" class = "mb-1">Nombre de usuario</label>
+                        <input type="username" name="username" class="form-control" aria-describedby="usernameHelp" placeholder="Ingrese nombre de usuario" id="username">
+                    </div>
+                    <div class="mb-4">
+                    <label for="password" class = "mb-1">Contraseña</label>
+                        <input type="password" name="password" class="form-control"  placeholder="Ingrese contraseña" id="password">
+                    </div>
+                    <div class="mb-4 form-check">
+                        <input type="checkbox" class="form-check-input " id="remember">
+                        <label class="form-check-label" for="remember">Recordarme</label>
+                    </div>
+                    <div class="d-grid">
+                        <button type = "submit" class = "btn btn-primary">Login</button>
+                    </div>  
+                    <div class="my-3">
+                        <span><a href="#">¿Contraseña olvidada?</a></span>
+                    </div>  
+                </form>
+                <br><br><br><br><br><br>
+                
+            </div>
+        </div>
+    </div>
+    
+</body>
 </html>
