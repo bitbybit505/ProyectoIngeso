6e5e0158111efe6815566b3436086c905d3628cd
SigmaBot <44101579+SigmaBot@users.noreply.github.com>
2023-07-20 20:53:37 -0400

    Detalles estéticos

--- add-user.php
+++ add-user.php
@@ -44,6 +44,20 @@
     .lateral-bar{
       background-color: #FFA07A;
     }
+
+    @media (max-width: 1368px){
+      .mb-bitbybit{
+        margin-bottom: 1px;
+      }
+
+      .mbb-bitbybit{
+        margin-top: 10px;
+      }
+
+      .pt-bitbybit{
+        padding-top: 7px !important;
+      }
+    }
   </style>
 
 <script>
@@ -174,37 +188,37 @@ $(document).ready(function () {
       <br>
     </nav>
     
-      <div class="container-fluid col-md-6 justify-content-center pt-1">
+      <div class="container-fluid col-md-6 justify-content-center pt-bitbybit">
         <div class="card">
           <div class="card-header fw-semibold ">Agregar Usuario</div>
           <div class="card-body">
             <form method="POST" action="database/validate-user.php" onsubmit="return validateForm();">
-              <div class="form-group fw-semibold">
+              <div class="form-group mb-bitbybit fw-semibold">
                 <label for="rut" class="mb-1">RUT</label>
                 <input type="text" class="form-control" id="rut" name="rut" placeholder="Ingrese su RUT" required>
               </div>
-              <div class="form-group fw-semibold">
+              <div class="form-group mb-bitbybit fw-semibold">
                 <label for="name" class="mb-1">Nombre</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Ingrese su Nombre" required>
               </div>
-              <div class="form-group fw-semibold">
+              <div class="form-group mb-bitbybit fw-semibold">
                 <label for="last_name" class="mb-1">Apellido</label>
                 <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Ingrese su Apellido" required>
               </div>
-              <div class="form-group fw-semibold">
+              <div class="form-group mb-bitbybit fw-semibold">
                 <label for="username" class="mb-1">Usuario</label>
                 <input type="text" class="form-control" id="username" name="username" placeholder="Ingrese su Nombre de Usuario" required>
               </div>
-              <div class="form-group fw-semibold">
+              <div class="form-group mb-bitbybit fw-semibold">
                 <label for="email" class="mb-1">Correo</label>
                 <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese su Correo" 
                 required pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}">
               </div>
-              <div class="form-group fw-semibold">
+              <div class="form-group mb-bitbybit fw-semibold">
                 <label for="password" class="mb-1">Contraseña</label>
                 <input type="password" class="form-control" id="password" name="password" placeholder="Ingrese Contraseña" required>
               </div>
-              <div class="form-group mb-2 fw-semibold">
+              <div class="form-group mb-bitbybit fw-semibold">
                 <label for="phoneNumber" >Número de teléfono</label>
                 <div class="input-group">
                   <div class="input-group-prepend">
@@ -215,7 +229,7 @@ $(document).ready(function () {
               </div>
 
               <!--<input type="hidden" name="table" value= "users" /> -->
-              <button type="submit" class="btn btn-primary mb-1">Agregar</button>
+              <button type="submit" class="btn btn-primary mbb-bitbybit">Agregar</button>
             </form>
             <?php
               if(isset($_SESSION['response'])){
